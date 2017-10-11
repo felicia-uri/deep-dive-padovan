6400d92ff5284293a3870e77ed63892c3f7a34ba
felicia-uri <felicia.uri@gmail.com>
2017-10-11 04:25:32 -0600

    Padovan implements Iterable

--- src/edu/cnm/deepdive/implementations/Padovan.java
+++ src/edu/cnm/deepdive/implementations/Padovan.java
@@ -7,42 +7,35 @@ import java.math.BigInteger;
 import java.util.Iterator;
 
 /**
- * @author Nicholas Bennett
+ * @author Felicia Urioste
  *
  */
-public class Fibonacci implements Iterable<BigInteger> {
+public class Padovan implements Iterable<BigInteger> {
+  
+  public Padovan() {
 
-  private static final int DEFAULT_UPPER = -1;
-  
-  private final int upper;
-  
-  public Fibonacci() {
-    this(DEFAULT_UPPER);
   }
   
-  public Fibonacci(int upper) {
-    this.upper = upper;
-  }
+
   
   @Override
   public Iterator<BigInteger> iterator() {
-    // return new FibIterator();
-    
-    // Anonymous class implementing the Iterator<BigInteger> interface.
-    return new Iterator<BigInteger>() {
 
-      private BigInteger previous = BigInteger.valueOf(-1);
-      private BigInteger current = BigInteger.ONE;
+    return new Iterator<BigInteger>() {
+      private BigInteger lessTwo = BigInteger.ZERO;
+      private BigInteger previous = BigInteger.ONE;
+      private BigInteger current = BigInteger.ZERO;
       private int index = 0;
           
       @Override
       public boolean hasNext() {
-        return upper < 0 || index < upper;
+        return true;
       }
 
       @Override
       public BigInteger next() {
-        BigInteger next = previous.add(current);
+        BigInteger next = lessTwo.add(previous);
+        lessTwo = previous;
         previous = current;
         current = next;
         index++;
